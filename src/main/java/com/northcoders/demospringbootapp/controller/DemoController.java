68fa50132f6ef0653ef5b9abf15fcd59010c217a
Amanda Gabriele Alves Ferreira <amanda@amandas-mbp.home>
2024-08-28 10:55:35 +0100

    feat: create person class

--- src/main/java/com/northcoders/demospringbootapp/controller/DemoController.java
+++ src/main/java/com/northcoders/demospringbootapp/controller/DemoController.java
@@ -1,8 +1,12 @@
 package com.northcoders.demospringbootapp.controller;
 
+import com.northcoders.demospringbootapp.model.People;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1")
@@ -11,6 +15,14 @@ public class DemoController {
     @GetMapping("/hello")
     public String greeting(){
         return "Hello there!";
+    }
+
+    @GetMapping("/people")
+    public List<People> getPeople() {
+        List<People> person = new ArrayList<>();
+        person.add(new People("Simon", 18, "simon@northcoders.com", "Liverpool", "Pasta"));
+        person.add(new People("Alex", 17, "alex@northcoders.com", "London", "Burger"));
+        return person;
     }
 
 }
---
+++ src/main/java/com/northcoders/demospringbootapp/model/People.java
@@ -0,0 +1,5 @@
+package com.northcoders.demospringbootapp.model;
+
+public record People (String name, int age, String emailAddress, String placeOfBirth, String favouriteFood){
+
+}
